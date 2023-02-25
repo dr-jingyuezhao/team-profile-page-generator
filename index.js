f0cc9a66edf167fc11c39eac227e198e7ebe5d7b
Jasmine <dr.jingyuezhao@outlook.com>
2023-02-25 16:39:30 +0000

    update the code for functions: prompt for manager, prompt for adding a new employee/engineer/intern, generate HTML by calling render function

--- index.js
+++ index.js
@@ -10,119 +10,151 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+let team = [];
+
 // Use Inquirer to gather information about the development team members and create objects for each team member
 // When a user starts the application, they're prompted to enter the team manager's: Name, Employee ID, Email address, Office number
-const initQuestions = [
+const managerQuestions = [
     {
-        type: 'input',
-        message: 'What is the name of your manager?',
-        name: 'managerName',
+        type: "input",
+        message: "What is the team manager's name?",
+        name: "name",
     },
     {
-        type: 'input',
-        message: 'What is the employee ID of your manager?',
-        name: 'managerID',
+        type: "input",
+        message: "What is the team manager's employee ID?",
+        name: "id",
     },
     {
-        type: 'input',
-        message: 'What is the email address of your manager?',
-        name: 'managerEmail',
+        type: "input",
+        message: "What is the team manager's email address?",
+        name: "email",
     },
     {
-        type: 'input',
-        message: 'What is the office number of your manager?',
-        name: 'managerOfficeNo',
+        type: "input",
+        message: "What is the team manager's office number?",
+        name: "office",
     },
 ];
 
 // When a user enters those requirements, the user is presented with a menu with the option to: Add an engineer, Add an intern, Finish building the team
 const menuOptions = [
     {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'options',
+        type: "list",
+        message: "What would you like to do?",
+        name: "option",
         choices: [
-            'Add an engineer',
-            'Add an intern',
-            'Finish building the team',
+            "Add an engineer",
+            "Add an intern",
+            "Finish building the team",
         ],
     },
 ];
 
-// When a user selects the engineer or intern option, the user is prompted to enter the following: Name, ID, Email
-const userQuestions = [
+// When a user selects the engineer option, the user is prompted to enter the following: Engineer's Name, ID, Email, GitHub username
+const engineerQuestions = [
     {
-        type: 'input',
-        message: 'What is your name?',
-        name: 'userName',
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "name",
     },
     {
-        type: 'input',
-        message: 'What is your employee ID?',
-        name: 'userID',
+        type: "input",
+        message: "What is the engineer's employee ID?",
+        name: "id",
     },
     {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'userEmail',
+        type: "input",
+        message: "What is the engineer's email address?",
+        name: "email",
     },
-];
-
-// Engineer's GitHub username
-const engineerQ = [
     {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'engGithub',
+        type: "input",
+        message: "What is the engineer's GitHub username?",
+        name: "github",
     },
 ];
 
-// Intern’s school
-const internQ = [
+// When a user selects the intern option, the user is prompted to enter the following: Intern’s name, ID, Email, school
+const internQuestions = [
     {
-        type: 'input',
-        message: 'What is the name of your school?',
-        name: 'internSchool',
+        type: "input",
+        message: "What is the intern's name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is the intern's employee ID?",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "What is the intern's email address?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is the intern's school?",
+        name: "school",
     },
 ];
 
-// and then taken back to the menu
+// Function to initialize the application with questions for the manager user
+function promptForManager() {
+    // Prompt for the manager
+    inquirer.prompt(managerQuestions).then(response => {
+        // populate manager info
+        let manager = new Manager(response.name, response.id, response.email, response.office);
+        team.push(manager);
+        promptForNextEmployee();
+    })
+}
 
-inquirer.prompt(initQuestions).then(response => {
-    // populate manager info
-    // promptForNextEmployee ()
-})
-
-const promptForNextEmployee = () => {
+// Function to prompt for adding a new employee
+function promptForNextEmployee() {
     inquirer.prompt(menuOptions).then(response => {
-        // if engineer
-        //    promptForEngineer
-        // else if intern
-        //    promptForIntern
-        // else
-        //    use the functionality from page-template to generate the team
+        if (response.option === "Add an engineer") {
+            // promptForEngineer
+            promptForEngineer();
+        }
+        else if (response.option === "Add an intern") {
+            // promptForIntern
+            promptForIntern();
+        }
+        else {
+            // use the provided variable outputPath and call the render function with the team array containing all employee objects
+            generateHTML(outputPath, render(team));
+        }
     })
 }
 
-const promptForEngineer = () => {
-    inquirer.prompt([{
-        //engineer questions
-    }]).then(response => {
-        // add new engineer to employees array
-        // promptForNextEmployee
+// Function to prompt for adding a new engineer
+function promptForEngineer() {
+    inquirer.prompt(engineerQuestions).then(response => {
+        // populate engineer info
+        let engineer = new Engineer(response.name, response.id, response.email, response.github);
+        // add new engineer to team array
+        team.push(engineer);
+        promptForNextEmployee();
     })
 }
 
-const promptForIntern = () => {
-    inquirer.prompt([{
-        //intern questions
-    }]).then(response => {
-        // add new intern to employees array
-        // promptForNextEmployee
+// Function to prompt for adding a new intern
+function promptForIntern() {
+    inquirer.prompt(internQuestions).then(response => {
+        // populate intern info
+        let intern = new Intern(response.name, response.id, response.email, response.school);
+        // add new intern to team array
+        team.push(intern);
+        promptForNextEmployee();
     })
 }
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-const buildPage = () => {
-// render(myArrayOfTeamMembers)
+// Function to generate a webpage/HTML file named team.html in the output folder
+function generateHTML(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('You have successfully generated a webpage named team.html in the output folder!')
+    );
+    // To exit the application
+    process.exit(0);
 }
